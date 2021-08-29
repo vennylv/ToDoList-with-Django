aac294827163372afba228cc315ed3fbd9d956b5
vennylv <vennylv@gmail.com>
2021-08-29 22:58:14 +0900

    2nd commit

--- ToDoList/my_to_do_app/models.py
+++ ToDoList/my_to_do_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class TOdo(models.Model):
+    content = models.CharField(max_length=255)
+    
--- ToDoList/my_to_do_app/views.py
+++ ToDoList/my_to_do_app/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+#def index(request):
+#    return HttpResponse("my_to_do_app first page")
 def index(request):
-    return HttpResponse("my_to_do_app first page")
+    return render(request, "my_to_do_app/index.html")
+
